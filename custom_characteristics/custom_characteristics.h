26bed5216da6e252ac631705569ddee5045bafc6
maccoylton <dave@maccoylton.com>
2019-11-11 17:37:13 +0000

    Added characteristic for WiFi Reset

--- custom_characteristics/custom_characteristics.h
+++ custom_characteristics/custom_characteristics.h
@@ -89,6 +89,17 @@
 ##__VA_ARGS__
 
 
+#define HOMEKIT_CHARACTERISTIC_CUSTOM_WIFI_RESET HOMEKIT_CUSTOM_UUID("F0000006")
+#define HOMEKIT_DECLARE_CHARACTERISTIC_CUSTOM_WIFI_RESET(_value, ...) \
+.type = HOMEKIT_CHARACTERISTIC_CUSTOM_WIFI_RESET, \
+.description = "Reset WiFi", \
+.format = homekit_format_bool, \
+.permissions = homekit_permissions_paired_read \
+| homekit_permissions_paired_write \
+| homekit_permissions_notify, \
+.value = HOMEKIT_BOOL_(_value), \
+##__VA_ARGS__
+
 #endif
 
 void save_characteristic_to_flash (homekit_characteristic_t *ch, homekit_value_t value);
